9489d4fc261356c3df235175e79daf92c24092c7
rgowan <rane_00@hotmail.com>
2015-04-13 15:58:47 +0100

    added full name field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413123724) do
+ActiveRecord::Schema.define(version: 20150413145756) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150413123724) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "full_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
