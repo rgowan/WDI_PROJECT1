4644a3effc80fc21bd51d00d989e7815897c27cb
rgowan <rane_00@hotmail.com>
2015-04-14 09:22:19 +0100

    started day 2 of project

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,15 @@
 class Song < ActiveRecord::Base
+  
+  # Uploader for Song Music File
   mount_uploader :music_file, SongUploader
+  # Uploader for Song Artwork
   mount_uploader :artwork, ArtworkUploader
+  
+  # Assosiation to Songs
   belongs_to :user
+
+  # User Validations
+  validates :title, length: { maximum: 20 }
 end
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Uploader for User Profile Picture
   mount_uploader :profile_picture, ProfileUploader
-  has_many :songs
+  # Assosiation to Songs
+  has_many :songs, dependent: :destroy
+
+  # User Validations
+  validates :full_name, presence: true, length: {in: 6..50}
+  validates :stage_name, presence: true, length: {in: 2..25}
 end
